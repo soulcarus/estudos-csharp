3a70e3ca1a73b3bf5d8af882a8b999bdf1828a3c
Ícaro Moreira <103120277+icarufc@users.noreply.github.com>
2022-10-28 11:38:19 -0300

    Program Calculadora

---
+++ ProgramCalculadora.cs
@@ -0,0 +1,13 @@
+using estudos-csharp.Models;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(10, 30);
+calc.Subtrair(10, 30);
+calc.Dividir(10, 30);
+calc.Multiplicar(10, 30);
+calc.Potencia(10,3);
+calc.Coseno(30);
+calc.Seno(30);
+calc.Tangente(30);
+calc.RaizQuadrada(30);
